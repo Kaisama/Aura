27682d17709412724d456209079fe3e54c210fbd
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2024-07-26 17:48:24 +0530

    Delete Backend directory

--- Backend/routes/twilio.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {sendMessage} from "../controllers/twilio.controller.js"
-
-const router = express.Router();
-
-router.post('/send-message', sendMessage);
-
-export default router;
